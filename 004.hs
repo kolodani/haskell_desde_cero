091f3b4132461e6f01f16eada55d4a592e5a46bf
kolodani <danielvinzia@gmail.com>
2022-08-22 15:36:32 -0300

    clase 4 de haskell

---
+++ 004.hs
@@ -0,0 +1,5 @@
+-- los nombres y los argumentos de funciones.
+--  deben comenzar con minusculas
+--  pueden contener letras en minusculas, mayusculas, digitos, guiones bajo, comillas simples.
+
+-- podemos pasar por agumento una lista
