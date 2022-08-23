8b035d0a5009e67281495e56830bbd040aaddba8
kolodani <danielvinzia@gmail.com>
2022-08-23 15:23:53 -0300

    clase 7 de haskell

---
+++ 007.hs
@@ -0,0 +1,10 @@
+module Test where
+
+-- usamos la notacion v :: T
+-- se puede leer v tiene el tipo T
+
+-- calcula el doble
+doble :: Int -> Int
+doble x = 2 * x
+
+-- una expresion debe tener un tipo que es calculado antes de su ejecucion
