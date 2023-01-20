07d0750ca5b929f2300bf42731768a36099ac4cb
kolodani <danielvinzia@gmail.com>
2023-01-20 17:23:31 -0300

    practica 004 de haskell para principiantes

---
+++ haskell_para_principiante/004.hs
@@ -0,0 +1,14 @@
+{-
+mas sobre listas
+-}
+
+-- suma todos los valores de una lista, la funcion que se usa es la sum
+lista1 = [1, 2, 3, 4, 5, 6]
+
+sumalista1 = sum lista1
+
+-- el producto de todos los valores de una lista, la funcion que se usa es product
+producto1 = product lista1
+
+-- ver si un elemento existe dentro de una lista, la funcion que se usa es elem
+existe1 = 5 `elem` lista1
