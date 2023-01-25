50199cbfa766bef21d9a64c0d380fe0eb7dc332d
kolodani <danielvinzia@gmail.com>
2023-01-25 15:29:36 -0300

    logica de primer orden en haskell, 001, concluido

--- logica_de_primer_orden_en_haskell/01/001.hs
+++ logica_de_primer_orden_en_haskell/01/001.hs
@@ -36,3 +36,93 @@ aplicafun f [] = []
 aplicafun f (x:xs) = f x : aplicafun f xs
 
 ejemplo1 = map (`divisiblePor` 2) [1,2,3,4,5,6,7,8,9,10]
+
+ejemplo2 = filter (<5) [1,5,7,2,3]
+
+aquellosQueCumplen :: (a -> Bool) -> [a] -> [a]
+aquellosQueCumplen p [] = []
+aquellosQueCumplen p (x:xs) | p x = x : aquellosQueCumplen p xs
+                            | otherwise = aquellosQueCumplen p xs
+
+functionG :: Int -> Int
+functionG x | x /= 0 = 5
+            | otherwise = 0
+
+
+-- N-uplas
+
+primerElemento :: (a, b) -> a
+primerElemento (x, _) = x
+
+segundoElemento :: (a, b) -> b
+segundoElemento (_, y) = y
+
+
+-- Conjuncion, disyuncion y cuantificacion
+
+conjuncion :: [Bool] -> Bool
+conjuncion [] = True
+conjuncion (x:xs) = x && conjuncion xs
+
+disyuncion :: [Bool] -> Bool
+disyuncion [] = False
+disyuncion (x:xs) = x || disyuncion xs
+
+algun, todos :: (a -> Bool) -> [a] -> Bool
+algun p = disyuncion . aplicafun p
+todos p = conjuncion . aplicafun p
+
+pertenece, noPertenece :: Eq a => a -> [a] -> Bool
+pertenece = algun . (==)
+noPertenece = todos . (/=)
+
+
+-- plegados especiales foldr y foldl
+
+factorial :: Int -> Int
+factorial 0 = 1
+factorial n = n * factorial (n-1)
+
+sumaLaLista :: Num a => [a] -> a
+sumaLaLista [] = 0
+sumaLaLista (x:xs) = x + sumaLaLista xs
+
+plegadoPorLaDerecha :: (a -> b -> b) -> b -> [a] -> b
+plegadoPorLaDerecha f z [] = z
+plegadoPorLaDerecha f z (x:xs) = f x (plegadoPorLaDerecha f z xs)
+
+conjuncion1, disyuncion1 :: [Bool] -> Bool
+conjuncion1 = plegadoPorLaDerecha (&&) True
+disyuncion1 = plegadoPorLaDerecha (||) False
+
+plegadoPorLaIzquierda :: (b -> a -> b) -> b -> [a] -> b
+plegadoPorLaIzquierda f z [] = z
+plegadoPorLaIzquierda f z (x:xs) = plegadoPorLaIzquierda f (f z x) xs
+
+listaInversa :: [a] -> [a]
+listaInversa = plegadoPorLaIzquierda (\xs x -> x:xs) []
+
+
+-- teoria de tipos
+divideEntre2 :: Fractional b => [b] -> [b]
+divideEntre2 xs = map (\x -> x / 2) xs
+
+data Entidades = A | B | C | D | E | F | G
+                | H | I | J | K | L | M | N
+                | O | P | Q | R | S | T | U
+                | V | W | X | Y | Z | Inespecifico
+    deriving (Eq, Bounded, Enum)
+
+instance Show Entidades where
+    show A = "A"; show B = "B"; show C = "C";
+    show D = "D"; show E = "E"; show F = "F";
+    show G = "G"; show H = "H"; show I = "I";
+    show J = "J"; show K = "K"; show L = "L";
+    show M = "M"; show N = "N"; show O = "O";
+    show P = "P"; show Q = "Q"; show R = "R";
+    show S = "S"; show T = "T"; show U = "U";
+    show V = "V"; show W = "W"; show X = "X";
+    show Y = "Y"; show Z = "Z"; show Inespecifico = "*"
+
+entidades :: [Entidades]
+entidades = [minBound .. maxBound]
