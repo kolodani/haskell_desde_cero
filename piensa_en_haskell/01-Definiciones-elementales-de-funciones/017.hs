0689298fdd8d62814b2450dcfb1e054bbac6b14c
kolodani <danielvinzia@gmail.com>
2023-01-27 17:26:27 -0300

    piensa en haskell, ejercicio 1.17

---
+++ piensa_en_haskell/01-Definiciones-elementales-de-funciones/017.hs
@@ -0,0 +1,8 @@
+{-
+    1.17
+    igualdad de 4 elementos
+-}
+
+dosIguales x y = x == y
+tresIguales x y z = dosIguales x y && dosIguales y z
+cuatroIguales x y z w = tresIguales x y z && dosIguales y z
