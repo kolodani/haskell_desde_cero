62e3b1887b772ce73e378ba57a4a2c74a0efae0e
kolodani <danielvinzia@gmail.com>
2023-01-27 17:24:54 -0300

    piensa en haskell, ejercicio 1.9

---
+++ piensa_en_haskell/01-Definiciones-elementales-de-funciones/009.hs
@@ -0,0 +1,9 @@
+{-
+    1.9.1
+    Rango de una lista
+-}
+
+rango :: [Int] -> [Int]
+rango xs = [minimum xs, maximum xs]
+-- minimum es la funcion minima de una lista
+-- maximum es la funcion maxima de una lista
