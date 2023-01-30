311f6795c8b15b9a80130b316250ea7fe1949894
kolodani <danielvinzia@gmail.com>
2023-01-30 10:58:57 -0300

    piensa en haskell, ejercicios actualizados

--- piensa_en_haskell/01-Definiciones-elementales-de-funciones/017.hs
+++ piensa_en_haskell/01-Definiciones-elementales-de-funciones/017.hs
@@ -3,6 +3,9 @@
     igualdad de 4 elementos
 -}
 
+dosIguales :: Eq a => a -> a -> Bool
 dosIguales x y = x == y
+tresIguales :: Eq a => a -> a -> a -> Bool
 tresIguales x y z = dosIguales x y && dosIguales y z
+cuatroIguales :: Eq a => a -> a -> a -> a -> Bool
 cuatroIguales x y z w = tresIguales x y z && dosIguales x w
--- piensa_en_haskell/01-Definiciones-elementales-de-funciones/018.hs
+++ piensa_en_haskell/01-Definiciones-elementales-de-funciones/018.hs
@@ -3,4 +3,5 @@
     Propiedad triangular
 -}
 
+triangular :: (Ord a, Num a) => a -> a -> a -> Bool
 triangular a b c = a + b >= c && a + c >= b && b + c >= a
