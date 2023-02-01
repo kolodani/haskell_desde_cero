7271e60acbfc71b33bf79b099b774fa6f160c5e3
kolodani <danielvinzia@gmail.com>
2023-02-01 16:25:58 -0300

    piensa en haskell, ejercicio 2.12.2

--- piensa_en_haskell/02-Definiciones-por-comprension/012.hs
+++ piensa_en_haskell/02-Definiciones-por-comprension/012.hs
@@ -8,3 +8,12 @@ ternasPitagoricas x = [(a, b, c) | a <- [1..x],
                                     b <- [a+1..x],
                                     c <- [x-a-b],
                                     a^2 + b^2 == c^2]
+
+
+{-
+      2.12.2
+-}
+
+euler9 :: Int
+euler9 = a*b*c
+      where (a,b,c) = head (ternasPitagoricas 1000)
