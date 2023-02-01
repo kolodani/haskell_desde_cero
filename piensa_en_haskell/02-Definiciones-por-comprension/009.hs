729d565faaeacb3a3100766f02a46f77767cb929
kolodani <danielvinzia@gmail.com>
2023-02-01 15:46:59 -0300

    piensa en haskell, ejercicio 2.09, actualizacion

--- piensa_en_haskell/02-Definiciones-por-comprension/009.hs
+++ piensa_en_haskell/02-Definiciones-por-comprension/009.hs
@@ -3,6 +3,7 @@
       Aproximacion del limite
 -}
 
+aproxLimSeno :: (Enum a, Floating a) => a -> [a]
 aproxLimSeno n = [sin(1/m)/(1/m) | m <- [1..n]]
 
 
@@ -10,4 +11,5 @@ aproxLimSeno n = [sin(1/m)/(1/m) | m <- [1..n]]
       2.9.2
 -}
 
+errorLimSeno :: (Enum a, Ord a, Floating a) => a -> a
 errorLimSeno x = head [m | m <- [1..], abs(1-sin(1/m)/(1/m)) < x]
