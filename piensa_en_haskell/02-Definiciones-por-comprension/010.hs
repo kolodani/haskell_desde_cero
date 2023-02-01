65cc3a8391dbd0e6be054e82b5b13afdaad384f6
kolodani <danielvinzia@gmail.com>
2023-02-01 15:51:09 -0300

    piensa en haskell, ejercicio 2.10.2

--- piensa_en_haskell/02-Definiciones-por-comprension/010.hs
+++ piensa_en_haskell/02-Definiciones-por-comprension/010.hs
@@ -5,3 +5,11 @@
 
 calculaPi :: (Enum a, Floating a) => a -> a
 calculaPi n = 4 * sum [(-1)**x/(2*x+1) | x <- [0..n]]
+
+
+{-
+      2.10.2
+-}
+
+errorPi :: (Enum a, Ord a, Floating a) => a -> a
+errorPi x = head [n | n <- [1..], abs (pi - calculaPi n) < x]
